92fe58ba2b132e5dacfd1e56e9ff90323823b0ca
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-07-16 12:44:38 +0200

    Add JS code structure for tags

--- js/script.js
+++ js/script.js
@@ -44,7 +44,8 @@ const titleClickHandler=function(event){
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
   console.log('Links generated');
@@ -85,3 +86,33 @@ function generateTitleLinks() {
   }
 }
 generateTitleLinks(); 
+
+function generateTags(){
+
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+  /* START LOOP: for each tag */
+
+    /* generate HTML of the link */
+
+    /* add generated code to html variable */
+
+  /* END LOOP: for each tag */
+
+  /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
+
